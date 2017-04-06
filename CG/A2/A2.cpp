3d21f56b667c8d8b32090f5783049781b5ac98c5
gaviotas <lmh315@yonsei.ac.kr>
2017-04-06 23:20:09 +0900

    2017-04-06 11:20

--- CG/A2/A2.cpp
+++ CG/A2/A2.cpp
@@ -16,21 +16,28 @@ static int mode_program = 0; // 0 : Draw Mode , 1 : Selection Mode
 static int mode_drawing = -1; // 0 : lines, 1 : rectangles, 2 : circles, 3 : triangle 
 
 struct line {
-	int vertex_s_x, vertex_s_y;
-	int vertex_f_x, vertex_f_y;
+	int x1, y1;
+	int x2, y2;
 	int drawing_state;
+
+	int t_x, t_y;
+
 };
 
 struct rect {
-	int vertex_x_1, vertex_y_1;
-	int vertex_x_2, vertex_y_2;
+	int x1, y1;
+	int x2, y2;
 	int drawing_state;
+
+	int t_x, t_y;
 };
 
 struct circle {
-	float rad;
+	double rad;
 	int x, y;
 	int drawing_state;
+
+	int t_x, t_y;
 };
 
 struct line line[100];
@@ -41,11 +48,65 @@ int num_line = 0;
 int num_rect = 0;
 int num_circle = 0;
 
-float get_distance(int x1, int y1, int x2, int y2) {
+int selected_shape = -1;
+int selected_num = -1;
+int selected_x, selected_y;
+double min_distance = 10;
+
+double get_distance(int x1, int y1, int x2, int y2) {
 
 	return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
 
 }
+
+void find_min_dist() {
+	// line part
+	for (int i = 0; i < num_line; i++) {
+		if (get_distance(line[i].x1, line[i].y1, mouseX, mouseY) < min_distance) {
+			min_distance = get_distance(line[i].x1, line[i].y1, mouseX, mouseY);
+			selected_num = i;
+			selected_shape = 0;
+			selected_x = line[i].x1;
+			selected_y = line[i].y1;
+		}
+		if (get_distance(line[i].x2, line[i].y2, mouseX, mouseY) < min_distance) {
+			min_distance = get_distance(line[i].x2, line[i].y2, mouseX, mouseY);
+			selected_num = i;
+			selected_shape = 0;
+			selected_x = line[i].x2;
+			selected_y = line[i].y2;
+		}
+	}
+	// rect part
+	for (int i = 0; i < num_rect; i++) {
+		if (get_distance(rect[i].x1, rect[i].y1, mouseX, mouseY) < min_distance) {
+			min_distance = get_distance(rect[i].x1, rect[i].y1, mouseX, mouseY);
+			selected_num = i;
+			selected_shape = 1;
+			selected_x = rect[i].x1;
+			selected_y = rect[i].y1;
+		}
+		if (get_distance(rect[i].x2, rect[i].y2, mouseX, mouseY) < min_distance) {
+			min_distance = get_distance(rect[i].x2, rect[i].y2, mouseX, mouseY);
+			selected_num = i;
+			selected_shape = 1;
+			selected_x = rect[i].x2;
+			selected_y = rect[i].y2;
+		}
+	}
+	// circle part
+	for (int i = 0; i < num_circle; i++) {
+		if (get_distance(circle[i].x, circle[i].y, mouseX, mouseY) < min_distance) {
+			min_distance = get_distance(circle[i].x, circle[i].y, mouseX, mouseY);
+			selected_num = i;
+			selected_shape = 2;
+			selected_x = circle[i].x;
+			selected_y = circle[i].y;
+		}
+	}
+}
+
+
 							// Track Window Dimensions
 void reshape(int w, int h)
 {
@@ -69,6 +130,7 @@ void keyboard(unsigned char key, int x, int y)
 		break;
 	case 's':
 		mode_program = 1;
+		mode_drawing = -1;
 		break;
 	case 'l' :
 		mode_drawing = 0;
@@ -102,26 +164,35 @@ void mousePress(GLint button, GLint state, GLint x, GLint y)
 		mouseX = x;
 		mouseY = winH - y;
 
-		switch (mode_drawing) {
-		case 0 :
-			line[num_line].drawing_state = 1;
+		if (mode_program == 0) {
+			switch (mode_drawing) {
+			case 0:
+				line[num_line].drawing_state = 1;
 
-			line[num_line].vertex_s_x = mouseX;
-			line[num_line].vertex_s_y = mouseY;
-			break;
-		case 1:
-			rect[num_rect].drawing_state = 1;
+				line[num_line].x1 = mouseX;
+				line[num_line].y1 = mouseY;
+				break;
+			case 1:
+				rect[num_rect].drawing_state = 1;
 
-			rect[num_rect].vertex_x_1 = mouseX;
-			rect[num_rect].vertex_y_1 = mouseY;
-			break;
-		case 2:
-			circle[num_circle].drawing_state = 1;
+				rect[num_rect].x1 = mouseX;
+				rect[num_rect].y1 = mouseY;
+				break;
+			case 2:
+				circle[num_circle].drawing_state = 1;
 
-			circle[num_circle].x = mouseX;
-			circle[num_circle].y = mouseY;
-			break;
+				circle[num_circle].x = mouseX;
+				circle[num_circle].y = mouseY;
+				break;
+			}
 		}
+
+		else if (mode_program == 1) {
+			find_min_dist();
+			printf("ok %d\n",selected_num);
+		}
+		
+
 		glutPostRedisplay();
 	}
 	if (button == GLUT_LEFT_BUTTON  && state == GLUT_UP)
@@ -129,28 +200,61 @@ void mousePress(GLint button, GLint state, GLint x, GLint y)
 		printf("LEFT Mouse Up\n");
 		mouseX = x;
 		mouseY = winH - y;
-		switch (mode_drawing) {
-		case 0 :
-			line[num_line].drawing_state = 0;
 
-			line[num_line].vertex_f_x = mouseX;
-			line[num_line].vertex_f_y = mouseY;
-			num_line++;
-			break;
-		case 1:
-			rect[num_rect].drawing_state = 0;
+		if (mode_program == 0) {
+			switch (mode_drawing) {
+			case 0:
+				line[num_line].drawing_state = 0;
 
-			rect[num_rect].vertex_x_2 = mouseX;
-			rect[num_rect].vertex_y_2 = mouseY;
-			num_rect++;
-			break;
-		case 2:
-			circle[num_circle].drawing_state = 0;
+				line[num_line].x2 = mouseX;
+				line[num_line].y2 = mouseY;
+				num_line++;
+				break;
+			case 1:
+				rect[num_rect].drawing_state = 0;
 
-			circle[num_circle].rad = get_distance(circle[num_circle].x, circle[num_circle].y, mouseX, mouseY);
-			num_circle++;
-			break;
+				rect[num_rect].x2 = mouseX;
+				rect[num_rect].y2 = mouseY;
+				num_rect++;
+				break;
+			case 2:
+				circle[num_circle].drawing_state = 0;
+
+				circle[num_circle].rad = get_distance(circle[num_circle].x, circle[num_circle].y, mouseX, mouseY);
+				num_circle++;
+				break;
+			}
 		}
+
+		else if (mode_program == 1) {
+			switch (selected_shape) {
+			case 0 : 
+				line[selected_num].x1 += line[selected_num].t_x;
+				line[selected_num].x2 += line[selected_num].t_x;
+				line[selected_num].y1 += line[selected_num].t_y;
+				line[selected_num].y2 += line[selected_num].t_y;
+				line[selected_num].t_x = 0;
+				line[selected_num].t_y = 0;
+			case 1:
+				rect[selected_num].x1 += rect[selected_num].t_x;
+				rect[selected_num].x2 += rect[selected_num].t_x;
+				rect[selected_num].y1 += rect[selected_num].t_y;
+				rect[selected_num].y2 += rect[selected_num].t_y;
+				rect[selected_num].t_x = 0;
+				rect[selected_num].t_y = 0;
+			case 2:
+				circle[selected_num].x += circle[selected_num].t_x;
+				circle[selected_num].y += circle[selected_num].t_y;
+				circle[selected_num].t_x = 0;
+				circle[selected_num].t_y = 0;
+			}
+			selected_shape = -1;
+			selected_num = -1;
+			selected_x = -1;
+			selected_y = -1;
+			min_distance = 10;
+		}
+
 		glutPostRedisplay();
 	}
 
@@ -178,9 +282,36 @@ void mousePress(GLint button, GLint state, GLint x, GLint y)
 // mousePress callback
 void mouseMotion(int x, int y)
 {
+
+	int move_x, move_y;
+
 	mouseX = x;
 	mouseY = winH - y;
-	printf("Mouse motion (%i %i) with a button down \n", mouseX, mouseY); // we don't know which button!  Need a global to record it from mousePress() function
+//	printf("Mouse motion (%i %i) with a button down \n", mouseX, mouseY); // we don't know which button!  Need a global to record it from mousePress() function
+
+	if (mode_program == 1) {
+		switch (selected_shape) {
+		case 0:
+			move_x = mouseX - selected_x;
+			move_y = mouseY - selected_y;
+			line[selected_num].t_x = move_x;
+			line[selected_num].t_y = move_y;
+			break;
+		case 1:
+			move_x = mouseX - selected_x;
+			move_y = mouseY - selected_y;
+			rect[selected_num].t_x = move_x;
+			rect[selected_num].t_y = move_y;
+			break;
+		case 2:
+			move_x = mouseX - selected_x;
+			move_y = mouseY - selected_y;
+			circle[selected_num].t_x = move_x;
+			circle[selected_num].t_y = move_y;
+			break;
+		}
+	}
+
 	glutPostRedisplay();
 }
 
@@ -192,31 +323,76 @@ void mousePassiveMotion(int x, int y)
 
 void draw_line(int i) {
 	glLineWidth(2.0);
+	glPushMatrix();
+	glTranslated(line[i].t_x, line[i].t_y, 0);
+
+	if (i == selected_num && selected_shape == 0) {
+		glColor3f(1, 0, 0);
+		glBegin(GL_LINE_LOOP);
+		glVertex2i(selected_x - 5, selected_y - 5);
+		glVertex2i(selected_x + 5, selected_y - 5);
+		glVertex2i(selected_x + 5, selected_y + 5);
+		glVertex2i(selected_x - 5, selected_y + 5);
+		glEnd();
+
+	}
+	glColor3f(0, 0, 0);
 	glBegin(GL_LINES);
-	glVertex2i(line[i].vertex_s_x, line[i].vertex_s_y);
+	glVertex2i(line[i].x1, line[i].y1);
 	if (line[i].drawing_state == 1) glVertex2i(mouseX, mouseY);
-	else glVertex2i(line[i].vertex_f_x, line[i].vertex_f_y);
+	else glVertex2i(line[i].x2, line[i].y2);
 	glEnd();
+	glPopMatrix();
 }
 void draw_rect(int i) {
 	glLineWidth(2.0);
+	glPushMatrix();
+	glTranslated(rect[i].t_x, rect[i].t_y, 0);
+
+	if (i == selected_num && selected_shape == 1) {
+		glColor3f(1, 0, 0);
+		glBegin(GL_LINE_LOOP);
+		glVertex2i(selected_x - 5, selected_y - 5);
+		glVertex2i(selected_x + 5, selected_y - 5);
+		glVertex2i(selected_x + 5, selected_y + 5);
+		glVertex2i(selected_x - 5, selected_y + 5);
+		glEnd();
+
+	}
+	glColor3f(0, 0, 0);
 	glBegin(GL_LINE_LOOP);
-	glVertex2i(rect[i].vertex_x_1, rect[i].vertex_y_1);
+	glVertex2i(rect[i].x1, rect[i].y1);
 	if (rect[i].drawing_state == 1)
 	{ 
-		glVertex2i(rect[i].vertex_x_1, mouseY);
+		glVertex2i(rect[i].x1, mouseY);
 		glVertex2i(mouseX, mouseY);
-		glVertex2i(mouseX, rect[i].vertex_y_1); 
+		glVertex2i(mouseX, rect[i].y1); 
 	}
 	else {
-		glVertex2i(rect[i].vertex_x_1, rect[i].vertex_y_2);
-		glVertex2i(rect[i].vertex_x_2, rect[i].vertex_y_2);
-		glVertex2i(rect[i].vertex_x_2, rect[i].vertex_y_1);
+		glVertex2i(rect[i].x1, rect[i].y2);
+		glVertex2i(rect[i].x2, rect[i].y2);
+		glVertex2i(rect[i].x2, rect[i].y1);
 	}
 	glEnd();
+	glPopMatrix();
 }
 void draw_circle(int i) {
 	glLineWidth(2.0);
+	glPushMatrix();
+	glTranslated(circle[i].t_x, circle[i].t_y, 0);
+
+	if (i == selected_num && selected_shape == 2) {
+		glColor3f(1, 0, 0);
+		glBegin(GL_LINE_LOOP);
+		glVertex2i(selected_x - 5, selected_y - 5);
+		glVertex2i(selected_x + 5, selected_y - 5);
+		glVertex2i(selected_x + 5, selected_y + 5);
+		glVertex2i(selected_x - 5, selected_y + 5);
+		glEnd();
+
+	}
+	glColor3f(0, 0, 0);
+
 	glBegin(GL_LINE_LOOP);
 	if (circle[i].drawing_state == 1) {
 		circle[i].rad = get_distance(mouseX, mouseY, circle[i].x, circle[i].y);
@@ -230,6 +406,7 @@ void draw_circle(int i) {
 		glVertex2f(circle[i].x+x, circle[i].y+y);
 	}
 	glEnd();
+	glPopMatrix();
 }
 void display()
 {
@@ -242,7 +419,7 @@ void display()
 /*
 	glPointSize(4.0);
 	glBegin(GL_POINTS);
-	glVertex2i(line.vertex_s_x, line.vertex_s_y);
+	glVertex2i(line.x1, line.y1);
 	glEnd();
 	*/
 	for (int j = 0; j < num_line+1; j++) {
@@ -268,11 +445,14 @@ int main(int argc, char** argv)
 {
 
 	for (int j = 0; j < 100; j++) {
-		line[j].vertex_s_x = -1;
-		line[j].vertex_s_y = -1;
-		line[j].vertex_f_x = -1;
-		line[j].vertex_f_y = -1;
+		line[j].x1 = -1;
+		line[j].y1 = -1;
+		line[j].x2 = -1;
+		line[j].y2 = -1;
 		line[j].drawing_state = 0;
+
+		line[j].t_x = 0;
+		line[j].t_y = 0;
 	}
 	glutInit(&argc, argv);   // not necessary unless on Unix
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
